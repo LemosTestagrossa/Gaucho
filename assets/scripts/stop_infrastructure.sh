c3585dd45849fb01116bc79b57c346a28cf6cee7
miguelemosreverte <miguellemos95@gmail.com>
2020-12-07 23:20:15 -0300

    cleanup of assets

--- assets/scripts/stop_infrastructure.sh
+++ assets/scripts/stop_infrastructure.sh
@@ -1,5 +1,4 @@
 docker-compose -f assets/docker-compose/docker-compose-cassandra.yml down -v
-docker-compose -f assets/docker-compose/docker-compose-scylla.yml down -v
 docker-compose -f assets/docker-compose/docker-compose-monitoring.yml down -v
 docker-compose -f assets/docker-compose/docker-compose-kafka.yml down -v
 
